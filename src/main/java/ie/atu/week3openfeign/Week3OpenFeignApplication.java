1080e8e986284d8290acca33556e969d98eb5bab
kenmchugh <G00267173@atu.ie>
2024-09-30 13:59:24 +0100

    openfeignPart1

---
+++ src/main/java/ie/atu/week3openfeign/Week3OpenFeignApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3OpenFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3OpenFeignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3openfeign/Week3OpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3OpenFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
